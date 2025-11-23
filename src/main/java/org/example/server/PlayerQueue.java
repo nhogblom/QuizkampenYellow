aa731631ef204d1b984ebea0d1b3c6e4d400f10d
Niklas Högblom <nhogblom@gmail.com>
2025-11-23 22:04:46 +0100

    first draft PlayerQueue

--- src/main/java/org/example/server/PlayerQueue.java
+++ src/main/java/org/example/server/PlayerQueue.java
@@ -1,5 +1,32 @@
 package org.example.server;
 
+import org.example.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
 public class PlayerQueue {
+    private List<Player> playerQueue = new ArrayList<>();
+
+    public void add(Player player) {
+        playerQueue.add(player);
+    }
+
+    public boolean enoughPlayersForAGame(){
+        return !playerQueue.isEmpty() && playerQueue.size() >= 2;
+    }
+
+    public List<Player> getPlayers() {
+        List<Player> players = new ArrayList<>();
+        if (playerQueue != null && playerQueue.size() >= 2){
+            players.add(playerQueue.removeFirst());
+            players.add(playerQueue.removeFirst());
+            return players;
+        }else{
+            return null;
+        }
+    }
+
     //TODO Håller spelare som väntar på en match och låter Matchmakern plocka ut två spelare åt gången.
 }
