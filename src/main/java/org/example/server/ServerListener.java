b9d4e43aa71e5541a03e335608bf677313444d4a
Niklas Högblom <nhogblom@gmail.com>
2025-11-23 22:05:58 +0100

    first draft ServerListener

--- src/main/java/org/example/server/ServerListener.java
+++ src/main/java/org/example/server/ServerListener.java
@@ -1,6 +1,42 @@
 package org.example.server;
 
-public class ServerListener {
+import org.example.Player;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerListener extends Thread {
+    private int port;
+    private PlayerQueue playerQueue;
+    private Matchmaker matchmaker;
+
+    public ServerListener(int port, PlayerQueue playerQueue, Matchmaker matchmaker) {
+        this.port = port;
+        this.playerQueue = playerQueue;
+        this.matchmaker = matchmaker;
+        this.start();
+    }
+
+    @Override
+    public void run() {
+        // Continuously receives new connections from players and adds them to playerQueue.
+        try (ServerSocket serverSocket = new ServerSocket(port)){
+            while (true) {
+                Socket socket = serverSocket.accept();
+                ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+                Player player = new Player(socket, objectInputStream, objectOutputStream);
+                playerQueue.add(player);
+                // wake matchmaker.
+                matchmaker.notifyAll();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
 
 //  TODO Lyssnar efter nya anslutningar. Skapar användare med användarnamn, socket, in- & out-ström.
