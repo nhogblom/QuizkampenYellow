9838df963c9721eaeadbdd3eb39c235339f65299
Niklas Högblom <nhogblom@gmail.com>
2025-11-23 21:52:17 +0100

    Added username, socket, oos ois to Player.java

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -1,6 +1,20 @@
 package org.example;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class Player {
+    private String username;
+    private Socket socket;
+    private ObjectInputStream objectInputStream;
+    private ObjectOutputStream objectOutputStream;
+
+    public Player(Socket socket, ObjectInputStream objectInputStream, ObjectOutputStream objectOutputStream) {
+        this.socket = socket;
+        this.objectInputStream = objectInputStream;
+        this.objectOutputStream = objectOutputStream;
+    }
 }
 
 // TODO Representerar en ansluten spelare; lagrar användarnamn och kommunikationsströmmar.
