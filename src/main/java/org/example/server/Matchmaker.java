d0559a89945d867a4556cb8db084e6ad4f5d2be3
Niklas Högblom <nhogblom@gmail.com>
2025-11-24 08:43:06 +0100

    Removed todo

--- src/main/java/org/example/server/Matchmaker.java
+++ src/main/java/org/example/server/Matchmaker.java
@@ -22,6 +22,3 @@ public class Matchmaker extends Thread {
         }
     }
 }
-
-
-// TODO Separat tråd som väcks när ny spelare läggs i kö. Plockar ut de två som väntat längst och startar ett spel.
