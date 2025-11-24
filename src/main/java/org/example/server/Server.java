52fa1e3556d823bb80cda4532654de1261e849fa
Niklas Högblom <nhogblom@gmail.com>
2025-11-24 10:19:37 +0100

    Removed hardcoded port.

--- src/main/java/org/example/server/Server.java
+++ src/main/java/org/example/server/Server.java
@@ -1,16 +1,14 @@
 package org.example.server;
 
 public class Server {
-    //todo ändra så att port tas från properties när en sådan är tillgänglig
-    private final int port = 55533;
     private final PlayerQueue playerQueue = new PlayerQueue();
-
+    private GameConfig gameConfig = new GameConfig();
 
     public Server() {
         // Creates matchmaker that runs within its own thread.
         Matchmaker matchmaker = new Matchmaker(playerQueue);
         // creates serverListener takes care of new connections.
-        ServerListener serverListener = new ServerListener(port, playerQueue, matchmaker);
+        ServerListener serverListener = new ServerListener(gameConfig.getPort(), playerQueue, matchmaker);
     }
 
     void main() {
