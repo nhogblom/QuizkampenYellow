c4e97a08bb9c7a3a1778fb222516ae1bd8ad3267
Niklas Högblom <nhogblom@gmail.com>
2025-11-25 08:17:47 +0100

    Changed variables from class to local.

--- src/main/java/org/example/server/Server.java
+++ src/main/java/org/example/server/Server.java
@@ -1,10 +1,11 @@
 package org.example.server;
 
 public class Server {
-    private final PlayerQueue playerQueue = new PlayerQueue();
-    private GameConfig gameConfig = new GameConfig();
 
     public Server() {
+        GameConfig gameConfig = new GameConfig();
+        PlayerQueue playerQueue = new PlayerQueue();
+
         // Creates matchmaker that runs within its own thread.
         Matchmaker matchmaker = new Matchmaker(playerQueue);
         // creates serverListener takes care of new connections.
