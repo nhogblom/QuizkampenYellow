0bb09f031a9e55ae566bc336761fdfcd08e5bb80
Jakob <jakob.b.geijer@gmail.com>
2025-11-25 20:18:07 +0100

    Added boxes for answerChecks at top of screen

--- src/main/java/org/example/client/panels/QuestionPanel.java
+++ src/main/java/org/example/client/panels/QuestionPanel.java
@@ -11,7 +11,6 @@ public class QuestionPanel extends JFrame {
     private String option3;
     private String option4;
 
-
     public QuestionPanel() {
         super("");
         setSize(600, 800);
@@ -21,13 +20,14 @@ public class QuestionPanel extends JFrame {
         setResizable(false);
         getContentPane().setBackground(Constants.DARK_BLUE);
 
+        addAnswerBoxes();
         addGuiComponents();
     }
 
     private void addGuiComponents(){
         JButton questions = new JButton(question);
         questions.setFont(new java.awt.Font("Arial", Font.BOLD, 16));
-        questions.setBounds(100, 100, 400, 200);
+        questions.setBounds(100, 140, 400, 200);
         questions.setHorizontalAlignment(SwingConstants.CENTER);
         getContentPane().add(questions);
         questions.setForeground(Color.BLACK);
@@ -62,6 +62,42 @@ public class QuestionPanel extends JFrame {
         opt4.setForeground(Color.BLACK);
         opt4.setBackground(Color.WHITE);
         add(opt4);
+    }
+
+    private void addAnswerBoxes(){
+        JButton answerCheckLeft[] = new JButton[3];
+        answerCheckLeft[0] = new JButton(option1);
+        answerCheckLeft[1] = new JButton(option1);
+        answerCheckLeft[2] = new JButton(option1);
+        answerCheckLeft[0].setBackground(Color.WHITE);
+        answerCheckLeft[1].setBackground(Color.WHITE);
+        answerCheckLeft[2].setBackground(Color.WHITE);
+        answerCheckLeft[0].setBounds(100, 40, 50, 50);
+        answerCheckLeft[1].setBounds(160, 40, 50, 50);
+        answerCheckLeft[2].setBounds(220, 40, 50, 50);
+        add(answerCheckLeft[0]);
+        add(answerCheckLeft[1]);
+        add(answerCheckLeft[2]);
+        answerCheckLeft[0].setEnabled(false);
+        answerCheckLeft[1].setEnabled(false);
+        answerCheckLeft[2].setEnabled(false);
+
+        JButton answerCheckRight[] = new JButton[3];
+        answerCheckRight[0] = new JButton(option1);
+        answerCheckRight[1] = new JButton(option1);
+        answerCheckRight[2] = new JButton(option1);
+        answerCheckRight[0].setBackground(Color.WHITE);
+        answerCheckRight[1].setBackground(Color.WHITE);
+        answerCheckRight[2].setBackground(Color.WHITE);
+        answerCheckRight[0].setBounds(330, 40, 50, 50);
+        answerCheckRight[1].setBounds(390, 40, 50, 50);
+        answerCheckRight[2].setBounds(450, 40, 50, 50);
+        add(answerCheckRight[0]);
+        add(answerCheckRight[1]);
+        add(answerCheckRight[2]);
+        answerCheckRight[0].setEnabled(false);
+        answerCheckRight[1].setEnabled(false);
+        answerCheckRight[2].setEnabled(false);
     }
 
     // todo frågorna visas upp och spelaren får göra sitt val.
