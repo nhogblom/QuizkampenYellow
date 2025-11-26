b5a66789e1824f4de3d712e48679db0578fc4d35
Shwky88 <seref-ozbodur@hotmail.com>
2025-11-26 12:41:55 +0100

    QuizCategory , QuizQuestion, QuestionRepository

--- src/main/java/org/example/server/QuizCategory.java
+++ src/main/java/org/example/server/QuizCategory.java
@@ -1,6 +1,18 @@
 package org.example.server;
 
 public class QuizCategory {
+    private String name;
+
+    public QuizCategory() {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return name;
+    }
 }
 
 // TODO Representerar en frågekategori och används för att organisera vilka frågor som hör till vilken kategori.
--- src/main/java/org/example/server/QuizQuestion.java
+++ src/main/java/org/example/server/QuizQuestion.java
@@ -2,4 +2,30 @@ package org.example.server;
 
 public class QuizQuestion {
     // TODO Representerar en enda quizfråga med frågetext, fyra svarsalternativ, rätt svar och kategori.
+
+    private String question;
+    private List<String> answers;
+    private int correctAnswerIndex;
+
+    public QuizQuestion() {
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswerIndex = correctAnswerIndex;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public List<String> getAnswers() {
+        return answers;
+    }
+
+    public int getCorrectAnswerIndex() {
+        return correctAnswerIndex;
+    }
+
+    public boolean isCorrect(int index) {
+        return index == correctAnswerIndex;
+    }
 }
