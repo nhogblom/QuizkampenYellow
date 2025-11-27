fa76813c93a006baa560eb082fbe5eebe8562d91
stoilsteve-hub <stoil.steve@me.com>
2025-11-27 18:19:55 +0100

    Removed commented code section // from Server.java

--- src/main/java/org/example/server/Server.java
+++ src/main/java/org/example/server/Server.java
@@ -3,14 +3,14 @@ package org.example.server;
 public class Server {
 
     public Server() {
-//        GameConfig gameConfig = new GameConfig();
+        GameConfig gameConfig = new GameConfig();
         PlayerQueue playerQueue = new PlayerQueue();
 
         // Creates matchmaker that runs within its own thread.
         Matchmaker matchmaker = new Matchmaker(playerQueue);
         // creates serverListener takes care of new connections.
-//        ServerListener serverListener = new ServerListener(gameConfig.getPort(), playerQueue);
-        ServerListener serverListener = new ServerListener(55554, playerQueue);
+        ServerListener serverListener = new ServerListener(gameConfig.getPort(), playerQueue);
+        //ServerListener serverListener = new ServerListener(55554, playerQueue);
     }
 
     void main() {
