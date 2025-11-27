8c966a6ca7907f36a09e63bca948789f0297d293
Niklas Högblom <nhogblom@gmail.com>
2025-11-27 16:06:25 +0100

    Changed sout output to clearly identify where the output came from.

--- src/main/java/org/example/server/Matchmaker.java
+++ src/main/java/org/example/server/Matchmaker.java
@@ -15,7 +15,7 @@ public class Matchmaker extends Thread {
     @Override
     public void run() {
         while (!this.isInterrupted()) {
-            System.out.println("Waiting for players to start");
+            System.out.println("Matchmaker: Waiting for players to start");
             List<Player> players = playerQueue.getPlayersForAGame();
             Game game = new Game(players.getFirst(),players.getLast());
             Thread t = new Thread(game);
