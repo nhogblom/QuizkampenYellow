bbd5b8421a0594375aae66de59db7c7d6fa80ba5
stoilsteve-hub <stoil.steve@me.com>
2025-11-30 12:09:08 +0100

    Prevent CHAT messages from entering game queue (temp until Game.java is complete)

--- src/main/java/org/example/server/PlayerListener.java
+++ src/main/java/org/example/server/PlayerListener.java
@@ -1,6 +1,7 @@
 package org.example.server;
 
 import org.example.Message;
+import org.example.MyMessageTypes;      //added import
 
 import java.io.IOException;
 import java.util.List;
@@ -21,13 +22,23 @@ public class PlayerListener extends Thread {
             try {
                 Object incoming = player.receive();
                 if (incoming instanceof Message msg) {
+
+                    // NEW: Keep CHAT out of queue
+
+                    if (msg.getType() == MyMessageTypes.CHAT) {
+                        System.out.println("CHAT message received (ignored for game queue): " + msg.getPayload());
+                        continue;  // do NOT queue chat messages
+                    }
+
                     incomingMessages.add(msg);
                     notifyAll();
-                } else if (incoming instanceof Player) {
 
+                } else if (incoming instanceof Player) {
                 }
+
             } catch (Exception e) {
-                System.out.println("Fel inträffade i inkommande dataström för spelare" + player.getUsername() + "\n" + e.getMessage());
+                System.out.println("Fel inträffade i inkommande dataström för spelare "
+                        + player.getUsername() + "\n" + e.getMessage());
                 this.interrupt();
                 break;
             }
@@ -36,27 +47,28 @@ public class PlayerListener extends Thread {
 
     public synchronized Message getGamePacket() {
         while (true) {
-        if (!incomingMessages.isEmpty()) {
-            return incomingMessages.removeFirst();
-        } else {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            if (!incomingMessages.isEmpty()) {
+                return incomingMessages.removeFirst();
+            } else {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
-        }
         }
     }
 
-//    public Message receive() {
-//        try {
-//            return (Message) player.getObjectInputStream().readObject();
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        } catch (ClassNotFoundException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
+
+    //    public Message receive() {
+    //        try {
+    //            return (Message) player.getObjectInputStream().readObject();
+    //        } catch (IOException e) {
+    //            throw new RuntimeException(e);
+    //        } catch (ClassNotFoundException e) {
+    //            throw new RuntimeException(e);
+    //        }
+    //    }
 
     public void send(Object object) {
         try {
