0e8a3f2d443ff70176ec9a85694cc58fe14d96a2
Niklas Högblom <nhogblom@gmail.com>
2025-11-30 10:31:28 +0100

    changed to CategoryPanel

--- src/main/java/org/example/client/panels/CategoryPanel.java
+++ src/main/java/org/example/client/panels/CategoryPanel.java
@@ -1,15 +1,20 @@
 package org.example.client.panels;
+
+import org.example.client.ClientBackpack;
+import org.example.server.CategoryPrompt;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class CategoryPanel extends JFrame {
 
-    private String category1;
-    private String category2;
-    private String category3;
+    private JButton cat1;
+    private JButton cat2;
+    private JButton cat3;
 
-    public CategoryPanel() {
+    public CategoryPanel(ClientBackpack backpack) {
         super("");
+        backpack.setCategoryPanel(this);
         setSize(600, 800);
         setLayout(null);
         setLocationRelativeTo(null);
@@ -17,10 +22,22 @@ public class CategoryPanel extends JFrame {
         setResizable(false);
         getContentPane().setBackground(Constants.DARK_BLUE);
 
-        addGuiComponents();;
+        addGuiComponents();
+        ;
     }
 
-    private void addGuiComponents(){
+    public void setCategories(CategoryPrompt categoryPrompt) {
+        cat1.setText(categoryPrompt.getCategories().get(0));
+        cat1.addActionListener((_)-> {
+            // skapa ett anrop tillbaka till servern~
+        });
+        cat2.setText(categoryPrompt.getCategories().get(1));
+        cat3.setText(categoryPrompt.getCategories().get(2));
+    }
+
+    public void waiting
+
+    private void addGuiComponents() {
         JLabel cat = new JLabel("Choose a category:");
         cat.setFont(new java.awt.Font("Arial", Font.BOLD, 36));
         cat.setBounds(100, 50, 400, 43);
@@ -28,21 +45,21 @@ public class CategoryPanel extends JFrame {
         getContentPane().add(cat);
 
         //Category 1
-        JButton cat1 = new JButton(category1);
+        cat1 = new JButton();
         cat1.setFont(new java.awt.Font("Arial", Font.BOLD, 16));
         cat1.setBounds(100, 200, 400, 43);
         cat1.setForeground(Color.BLACK);
         cat1.setBackground(Constants.LIGHT_GREEN);
         add(cat1);
         //Category 2
-        JButton cat2 = new JButton(category2);
+        cat2 = new JButton();
         cat2.setFont(new java.awt.Font("Arial", Font.BOLD, 16));
         cat2.setBounds(100, 300, 400, 43);
         cat2.setForeground(Color.BLACK);
         cat2.setBackground(Constants.LIGHT_GREEN);
         add(cat2);
         //Category 3
-        JButton cat3 = new JButton(category3);
+        cat3 = new JButton();
         cat3.setFont(new java.awt.Font("Arial", Font.BOLD, 16));
         cat3.setBounds(100, 400, 400, 43);
         cat3.setForeground(Color.BLACK);
