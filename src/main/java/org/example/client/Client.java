5c8d4db00940220cde4f7396595d33f8ea290bc6
Niklas Högblom <nhogblom@gmail.com>
2025-12-01 14:23:32 +0100

    removed unused stuff.

--- src/main/java/org/example/client/Client.java
+++ src/main/java/org/example/client/Client.java
@@ -8,38 +8,18 @@ import org.example.client.panels.RoundSummaryPanel;
 import javax.swing.*;
 
 public class Client {
-    private ClientBackpack backpack = new ClientBackpack();
-    private CategoryPanel categoryPanel = new CategoryPanel(backpack);
-    private QuestionPanel questionPanel = new QuestionPanel(backpack);
-    private RoundSummaryPanel roundSummaryPanel = new RoundSummaryPanel(backpack);
+    private final ClientBackpack backpack = new ClientBackpack();
+    private final CategoryPanel categoryPanel = new CategoryPanel(backpack);
+    private final QuestionPanel questionPanel = new QuestionPanel(backpack);
+    private final RoundSummaryPanel roundSummaryPanel = new RoundSummaryPanel(backpack);
 
     //TODO lägga in alla paneler här och sen bara styra om dom syns eller ej senare i programmet.
 
     public Client() {
         backpack.setCategoryPanel(categoryPanel);
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new MainWindow(backpack).setVisible(true);
-            }
-        });
+        SwingUtilities.invokeLater(() -> new MainWindow(backpack).setVisible(true));
     }
 
     void main() {
-
-
-
-
-//        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in))) {
-//            String input;
-//            while ((input = console.readLine()) != null) {
-//                if (input.equalsIgnoreCase("exit")) break;
-//                client.sendMessage(new Message(MyMessageTypes.CHAT,input));
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } finally {
-//            client.disconnect();
-//        }
     }
 }
