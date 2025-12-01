ecc92d7979019a49d1cbf79af92a32cf25e05e41
Niklas Högblom <nhogblom@gmail.com>
2025-12-01 14:24:57 +0100

    removed unused stuff.

--- src/main/java/org/example/client/Client.java
+++ src/main/java/org/example/client/Client.java
@@ -9,13 +9,13 @@ import javax.swing.*;
 
 public class Client {
     private final ClientBackpack backpack = new ClientBackpack();
-    private final CategoryPanel categoryPanel = new CategoryPanel(backpack);
     private final QuestionPanel questionPanel = new QuestionPanel(backpack);
     private final RoundSummaryPanel roundSummaryPanel = new RoundSummaryPanel(backpack);
 
     //TODO lägga in alla paneler här och sen bara styra om dom syns eller ej senare i programmet.
 
     public Client() {
+        CategoryPanel categoryPanel = new CategoryPanel(backpack);
         backpack.setCategoryPanel(categoryPanel);
         SwingUtilities.invokeLater(() -> new MainWindow(backpack).setVisible(true));
     }
