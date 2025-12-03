8b38cc0a96937e4a0b23d6bfc3a33b63a0f145eb
Niklas Högblom <nhogblom@gmail.com>
2025-12-03 10:33:02 +0100

    Removed LoginPanel.java

--- src/main/java/org/example/client/panels/LoginPanel.java
+++
@@ -1,8 +0,0 @@
-package org.example.client.panels;
-import javax.swing.*;
-import java.awt.*;
-
-public class LoginPanel extends JFrame {
-
-}
-// TODO Login screen, användaren promtas  för användarnamn & spelläge
